076f39820d32afedb9cac2916045da5433f9510c
742161455@qq.com <742161455@qq.com>
2020-02-09 21:21:30 +0800

    gin controllers Object-oriented

--- tool/new/gin_tmp.go
+++ tool/new/gin_tmp.go
@@ -2,7 +2,7 @@ package new
 
 func GinInit() {
 	fc1 := &FileContent{
-		FileName: "user.go",
+		FileName: "demo.go",
 		Dir:      "internal/transports/http/controllers",
 		Content: `package controllers
 
@@ -14,11 +14,16 @@ import (
 	"{{PROPATH}}{{service_name}}/internal/app/dto"
 )
 
-func Demo(c *gin.Context) {
+type DemoController struct {
+	infra *infra.Infra
+
+	UserSvc *service.UserService
+}
+
+func (this *DemoController) Demo(c *gin.Context) {
 
 	username := c.GetString("username")
-	svc := service.NewUserSvc(infra.NewInfra())
-	info := svc.GetUserInfo(c.Request.Context(), username)
+	info := this.UserSvc.GetUserInfo(c.Request.Context(), username)
 
 	c.JSON(http.StatusOK, gin.H{
 		"data": dto.NewUser(info),
@@ -26,11 +31,30 @@ func Demo(c *gin.Context) {
 }
 
 
-func Esim(c *gin.Context)  {
-	inf := infra.NewInfra()
+type PingController struct {
+	infra *infra.Infra
+}
 
+func (this *PingController) Ping(c *gin.Context)  {
+	errs := this.infra.HealthCheck()
+
+	if len(errs) > 0{
+		for _, err := range errs{
+			infra.NewInfra().Logger.Errorf(err.Error())
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{})
+	}else{
+		c.JSON(http.StatusOK, gin.H{})
+	}
+}
+
+type EsimController struct {
+	infra *infra.Infra
+}
+
+func (this *EsimController) Esim(c *gin.Context)  {
 	c.JSON(http.StatusOK, gin.H{
-		"Esim" : inf.String(),
+		"Esim" : this.infra.String(),
 	})
 }
 `,
@@ -48,10 +72,12 @@ import (
 )
 
 
-func RegisterGinServer(en *gin.Engine)  {
-	en.GET("/", controllers.Esim)
+func RegisterGinServer(en *gin.Engine, controllers *controllers.Controllers)  {
+	en.GET("/", controllers.Esim.Esim)
 
-	en.GET("/demo", controllers.Demo)
+	en.GET("/demo", controllers.Demo.Demo)
+
+	en.GET("/ping", controllers.Ping.Ping)
 }
 `,
 	}
@@ -70,7 +96,9 @@ import (
 	"github.com/jukylin/esim/container"
 	"{{PROPATH}}{{service_name}}/internal/transports/http/routers"
 	"github.com/jukylin/esim/middle-ware"
-	"gitlab.etcchebao.cn/go_service/esim/pkg/log"
+	"github.com/jukylin/esim/log"
+	"{{PROPATH}}{{service_name}}/internal/transports/http/controllers"
+	"{{PROPATH}}{{service_name}}/internal"
 )
 
 type GinServer struct{
@@ -81,11 +109,13 @@ type GinServer struct{
 	logger log.Logger
 
 	server *http.Server
+
+	app *{{service_name}}.App
 }
 
-func NewGinServer(esim *container.Esim) *GinServer {
+func NewGinServer(app *{{service_name}}.App) *GinServer {
 
-	httpport := esim.Conf.GetString("httpport")
+	httpport := app.Conf.GetString("httpport")
 
 	in := strings.Index(httpport, ":")
 	if in < 0 {
@@ -94,24 +124,25 @@ func NewGinServer(esim *container.Esim) *GinServer {
 
 	en := gin.Default()
 
-	if esim.Conf.GetString("runmode") != "pro"{
+	if app.Conf.GetString("runmode") != "pro"{
 		gin.SetMode(gin.DebugMode)
 	}else{
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	if esim.Conf.GetBool("http_tracer") == true{
-		en.Use(middle_ware.GinTracer(esim.Tracer))
+	if app.Conf.GetBool("http_tracer") == true{
+		en.Use(middle_ware.GinTracer(app.Tracer))
 	}
 
-	if esim.Conf.GetBool("http_metrics") == true {
+	if app.Conf.GetBool("http_metrics") == true {
 		en.Use(middle_ware.GinMonitor())
 	}
 
 	server := &GinServer{
 		en : en,
 		addr : httpport,
-		logger: esim.Logger,
+		logger: app.Logger,
+		app: app,
 	}
 
 	return server
@@ -119,7 +150,7 @@ func NewGinServer(esim *container.Esim) *GinServer {
 
 
 func (this *GinServer) Start(){
-	routers.RegisterGinServer(this.en)
+	routers.RegisterGinServer(this.en, controllers.NewControllers(this.app))
 
 	server := &http.Server{Addr: this.addr, Handler: this.en}
 	this.server = server
@@ -166,9 +197,9 @@ func TestMain(m *testing.M) {
 
 	app = {{service_name}}.NewApp()
 
-	app.Trans = append(app.Trans, http.NewGinServer(app.Esim))
-
 	app.Infra = infra.NewInfra()
+
+	app.Trans = append(app.Trans, http.NewGinServer(app))
 
 	app.Start()
 	errs := app.Infra.HealthCheck()
@@ -209,5 +240,127 @@ func TestControllers_Esim(t *testing.T)  {
 `,
 	}
 
-	Files = append(Files, fc1, fc2, fc3, fc4)
+	fc5 := &FileContent{
+		FileName: "controllers.go",
+		Dir:      "internal/transports/http/controllers",
+		Content: `package controllers
+
+import (
+	"{{PROPATH}}{{service_name}}/internal"
+	"github.com/google/wire"
+	"{{PROPATH}}{{service_name}}/internal/app/service"
+)
+
+
+
+type Controllers struct {
+
+	App *{{service_name}}.App
+
+	Ping *PingController
+
+	Esim *EsimController
+
+	Demo *DemoController
+
+}
+
+
+var controllersSet = wire.NewSet(
+	wire.Struct(new(Controllers), "*"),
+	providePingController,
+	provideEsimController,
+	provideDemoController,
+)
+
+
+func NewControllers(app *{{service_name}}.App) *Controllers {
+	controllers := initControllers(app)
+	return controllers
+}
+
+
+func providePingController(app *{{service_name}}.App) *PingController {
+	pingController := &PingController{}
+	pingController.infra = app.Infra
+	return pingController
+}
+
+
+func provideEsimController(app *{{service_name}}.App) *EsimController {
+	esimController := &EsimController{}
+	esimController.infra = app.Infra
+	return esimController
+}
+
+
+func provideDemoController(app *{{service_name}}.App) *DemoController {
+
+	userSvc := service.NewUserSvc(app.Infra)
+
+	demoController := &DemoController{}
+	demoController.infra = app.Infra
+	demoController.UserSvc = userSvc
+
+	return demoController
+}
+`,
+	}
+
+
+	fc6 := &FileContent{
+		FileName: "wire.go",
+		Dir:      "internal/transports/http/controllers",
+		Content: `//+build wireinject
+
+package controllers
+
+import (
+	"github.com/google/wire"
+	"{{PROPATH}}{{service_name}}/internal"
+)
+
+
+
+func initControllers(app *test2.App) *Controllers {
+	wire.Build(controllersSet)
+	return nil
+}
+`,
+	}
+
+
+	fc7 := &FileContent{
+		FileName: "wire_gen.go",
+		Dir:      "internal/transports/http/controllers",
+		Content: `// Code generated by Wire. DO NOT EDIT.
+
+//go:generate wire
+//+build !wireinject
+
+package controllers
+
+import (
+	"{{PROPATH}}{{service_name}}/internal"
+)
+
+// Injectors from wire.go:
+
+func initControllers(app *test2.App) *Controllers {
+	pingController := providePingController(app)
+	esimController := provideEsimController(app)
+	demoController := provideDemoController(app)
+	controllers := &Controllers{
+		App:  app,
+		Ping: pingController,
+		Esim: esimController,
+		Demo: demoController,
+	}
+	return controllers
+}
+
+`,
+	}
+
+	Files = append(Files, fc1, fc2, fc3, fc4, fc5, fc6, fc7)
 }
