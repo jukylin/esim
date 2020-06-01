930ed6873769d258abdc7cf8c9c29059032faebb
742161455@qq.com <742161455@qq.com>
2020-06-02 00:35:58 +0800

    Improve documentation

--- tool/new/domain_fc.go
+++ tool/new/domain_fc.go
@@ -7,7 +7,6 @@ var (
 		Content: `package entity
 
 type User struct {
-
 	// ID
 	ID int {{.SingleMark}}gorm:"column:id;primary_key"{{.SingleMark}}
 
@@ -17,6 +16,18 @@ type User struct {
 	// pwd
 	PassWord string {{.SingleMark}}gorm:"column:pass_word"{{.SingleMark}}
 }
+
+func (u User) CheckPwd(pwd string) bool {
+	if pwd == "" {
+		return false
+	}
+
+	if u.PassWord == pwd {
+		return true
+	}
+
+	return false
+}
 `,
 	}
 
@@ -25,8 +36,53 @@ type User struct {
 		Dir:      "internal/domain/user/service",
 		Content:  `domain service`,
 	}
+
+	domainfc3 = &FileContent{
+		FileName: "user_test.go",
+		Dir:      "internal/domain/user/entity",
+		Content: `package entity
+
+import "testing"
+
+func TestUser_CheckPwd(t *testing.T) {
+	type fields struct {
+		ID       int
+		UserName string
+		PassWord string
+	}
+	type args struct {
+		pwd string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{"check right", fields{1, "test1", "123456"},
+			args{"123456"}, true},
+		{"check error", fields{1, "test1", "123456"},
+			args{"111111"}, false},
+		{"empty pwd", fields{1, "test1", "123456"},
+			args{""}, false},
+			}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := User{
+				ID:       tt.fields.ID,
+				UserName: tt.fields.UserName,
+				PassWord: tt.fields.PassWord,
+			}
+			if got := u.CheckPwd(tt.args.pwd); got != tt.want {
+				t.Errorf("User.CheckPwd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+`,
+	}
 )
 
 func initDomainFiles() {
-	Files = append(Files, domainfc1, domainfc2)
+	Files = append(Files, domainfc1, domainfc2, domainfc3)
 }
